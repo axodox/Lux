e1fc2f0764c5fe766402ee2b6e7d0cf9d3018059
axodox <axodox@outlook.com>
2020-04-30 00:15:48 +0200

    Added devices and contexts

--- Lux.Service/pch.h
+++ Lux.Service/pch.h
@@ -1,6 +1,13 @@
 ﻿#pragma once
 
+#include <thread>
+#include <array>
+#include <tuple>
+#include <unordered_map>
+
 #include <dxgi1_6.h>
 #include <d3d11_4.h>
+
+#pragma comment(lib, "d3d11.lib")
 
 #include <winrt/Windows.Foundation.h>
