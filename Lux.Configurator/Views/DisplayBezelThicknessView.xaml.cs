1c9f6deb08fe60b7a68e4a04b7741930a8068c83
axodox <axodox@outlook.com>
2020-05-10 14:41:29 +0200

    Added layout configuration

---
+++ Lux.Configurator/Views/DisplayBezelThicknessView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Lux.Configurator.Framework;
+using Lux.Configurator.ViewModels;
+
+namespace Lux.Configurator.Views
+{
+  class DisplayBezelThicknessViewBase : ViewBase<DisplayBezelThicknessViewModel> { }
+
+  sealed partial class DisplayBezelThicknessView : DisplayBezelThicknessViewBase
+  {
+    public DisplayBezelThicknessView()
+    {
+      InitializeComponent();
+    }
+  }
+}
