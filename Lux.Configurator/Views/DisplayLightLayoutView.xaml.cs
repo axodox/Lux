012d9afbceb58dada526317e0da9f21fba74209f
Peter Major <peter.major@cae.com>
2020-05-10 14:41:29 +0200

    Added layout configuration

---
+++ Lux.Configurator/Views/DisplayLightLayoutView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Lux.Configurator.Framework;
+using Lux.Configurator.ViewModels;
+
+namespace Lux.Configurator.Views
+{
+  class DisplayLightLayoutViewBase : ViewBase<DisplayLightLayoutViewModel> { }
+
+  sealed partial class DisplayLightLayoutView : DisplayLightLayoutViewBase
+  {
+    public DisplayLightLayoutView()
+    {
+      ViewModel = new DisplayLightLayoutViewModel();
+      InitializeComponent();
+    }
+  }
+}
