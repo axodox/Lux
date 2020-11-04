fc1d77acd355c073eb70564c2ffef14a4772247c
Peter Major <peter.major@cae.com>
2020-11-05 00:53:51 +0100

    Added bottom toolbars

--- Lux/MainPage.h
+++ Lux/MainPage.h
@@ -4,20 +4,15 @@
 
 namespace winrt::Lux::implementation
 {
-    struct MainPage : MainPageT<MainPage>
-    {
-        MainPage();
-
-        int32_t MyProperty();
-        void MyProperty(int32_t value);
-
-        void ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
-    };
+  struct MainPage : MainPageT<MainPage>
+  {
+    MainPage();
+  };
 }
 
 namespace winrt::Lux::factory_implementation
 {
-    struct MainPage : MainPageT<MainPage, implementation::MainPage>
-    {
-    };
+  struct MainPage : MainPageT<MainPage, implementation::MainPage>
+  {
+  };
 }
